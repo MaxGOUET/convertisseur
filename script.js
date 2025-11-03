f320a9edf5b0d5eb22a201c7882f547716140e29
MaxGOUET <maxime.gouet1@gmail.com>
2025-11-03 15:34:05 +0100

    v2 - instant modification des valeurs

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // on recupére les données du html
 
-let toConvertValue = document.getElementById("toConvertValue");
-console.log(toConvertValue);
+let toConvertValueOne = document.getElementById("toConvertValueOne");
+console.log(toConvertValueOne);
+
+let toConvertValueTwo = document.getElementById("toConvertValueTwo");
+console.log(toConvertValueTwo);
 
 let valueOne = document.getElementById("valueOne");
 console.log(valueOne);
@@ -9,58 +12,45 @@ console.log(valueOne);
 let valueTwo = document.getElementById("valueTwo");
 console.log(valueTwo);
 
-let zoneSubmit = document.getElementById("convert");
-console.log(zoneSubmit);
-
-let zoneResult = document.querySelector(".showResult");
-console.log(zoneResult);
-
 // on empeche le comportement par défaut et on affiche le resultat
 function showResult() {
-  let sendForm = document.querySelector(".form");
-  sendForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const result = toConvertValue.value;
+  toConvertValueOne.addEventListener("keyup", (event) => {
+    const result = toConvertValueOne.value;
     validateConvert(result);
-    calculation();
+    calculation(event.target);
+    if (toConvertValueTwo.value == "NaN") {
+      toConvertValueOne.style.color = "red";
+    } else {
+      toConvertValueOne.style.color = "black";
+    }
+  });
+  toConvertValueTwo.addEventListener("keyup", (event) => {
+    const result = toConvertValueTwo.value;
+    validateConvert(result);
+    calculation(event.target);
+    if (toConvertValueOne.value == "NaN") {
+      toConvertValueTwo.style.color = "red";
+    } else {
+      toConvertValueTwo.style.color = "black";
+    }
   });
 }
 // on mets des régles concernant les caractères acceptés et on renvoi la valeur de toConvertValue
 function validateConvert(result) {
   let regex = new RegExp("[0-9.,]+");
   if (!(resultConvert = regex.test(result))) {
-    console.log("erreur");
-  } else {
-    console.log(toConvertValue.value);
+    toConvertValueOne.value = "";
+    toConvertValueTwo.value = "";
   }
 }
 
-// on force le changement des zone select euro et dollar
-function autoChangeEuroDollar() {
-  valueOne.addEventListener("change", (event) => {
-    if (!(valueOne.value == "euro")) {
-      valueTwo.value = "euro";
-    } else {
-      valueTwo.value = "dollar";
-    }
-  });
-
-  valueTwo.addEventListener("change", (event) => {
-    if (!(valueTwo.value == "dollar")) {
-      valueOne.value = "dollar";
-    } else {
-      valueOne.value = "euro";
-    }
-  });
-}
-
 // on calcule et on affiche le resultat dans la zone showResult
-function calculation() {
-  if (valueOne.value == "euro") {
-    let resultDollar = toConvertValue.value * "1.15";
-    zoneResult.innerText = Math.round(resultDollar * 100) / 100 + "$";
+function calculation(target) {
+  if (target.id == "toConvertValueOne") {
+    let resultDollar = toConvertValueOne.value * "1.15";
+    toConvertValueTwo.value = Math.round(resultDollar * 100) / 100;
   } else {
-    let resultEuro = toConvertValue.value / "1.15";
-    zoneResult.innerText = Math.round(resultEuro * 100) / 100 + "€";
+    let resultEuro = toConvertValueTwo.value / "1.15";
+    toConvertValueOne.value = Math.round(resultEuro * 100) / 100;
   }
 }
